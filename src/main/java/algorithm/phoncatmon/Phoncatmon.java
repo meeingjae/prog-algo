06460a5780bc48cc444f8a070f6a9b2bde1ff10f
Minjae Kim <minjae.kim@mytrum.com>
2023-01-17 23:00:23 +0900

    1 : 폰켓몬 리펙토링 및 결과 문서 생성

--- src/main/java/algorithm/phoncatmon/Phoncatmon.java
+++ src/main/java/algorithm/phoncatmon/Phoncatmon.java
@@ -7,8 +7,6 @@ public class Phoncatmon {
 
     // n 마리의 포켓몬이 있다
     public int solution(int[] nums) {
-
-        int answer = 0;
         // n 마리의 포켓몬 중 최대 절반의 포켓몬을 가져갈 수 있다
         int availableCnt = nums.length / 2;
         // n 마리의 포켓몬 중 같은 종류의 포켓몬은 가져갈 수 없다
@@ -17,10 +15,8 @@ public class Phoncatmon {
             numSet.add(num);
         }
         if (numSet.size() > availableCnt) {
-            answer = availableCnt;
-        } else {
-            answer = numSet.size();
+            return availableCnt;
         }
-        return answer;
+        return numSet.size();
     }
 }
