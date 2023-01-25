4766e5939365c168b6b3a046738a534e505377e3
Minjae Kim <minjae.kim@mytrum.com>
2023-01-25 21:46:48 +0900

    feat: 4. 같은 숫자는 싫어 구현

--- src/main/java/algorithm/hatesamenumber/HateSameNumber.java
+++ src/main/java/algorithm/hatesamenumber/HateSameNumber.java
@@ -1,9 +1,19 @@
 package algorithm.hatesamenumber;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class HateSameNumber {
 
     public static int[] solution(int[] source) {
-
-        return new int[] {};
+        int saveNum = -1;
+        final List<Integer> result = new ArrayList<>();
+        for(int a : source) {
+            if (saveNum != a) {
+                result.add(a);
+                saveNum = a;
+            }
+        }
+        return result.stream().mapToInt(it -> it).toArray();
     }
 }
