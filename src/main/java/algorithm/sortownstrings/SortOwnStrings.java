d32481a548d6ea509300024de5e7be741f870293
Minjae Kim <minjae.kim@mytrum.com>
2023-02-02 22:48:46 +0900

    feat: 7. 문자열 내 마음대로 정렬하기

--- src/main/java/algorithm/sortownstrings/SortOwnStrings.java
+++ src/main/java/algorithm/sortownstrings/SortOwnStrings.java
@@ -1,9 +1,18 @@
 package algorithm.sortownstrings;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class SortOwnStrings {
 
     public static String[] solution(String[] source, int n) {
 
-        return new String[] {};
+        List<String> target = new ArrayList<>();
+        for (String s : source) {
+            target.add(s.charAt(n) + s);
+        }
+        Collections.sort(target);
+        return target.stream().map(it -> it.substring(1)).toList().toArray(String[]::new);
     }
 }
