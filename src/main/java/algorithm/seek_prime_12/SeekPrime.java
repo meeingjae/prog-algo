cf567f4834e81b5430d7b1ea0873c4e035d0211a
Minjae Kim <minjae.kim@mytrum.com>
2023-02-11 12:17:31 +0900

    feat: 12. 소수 찾기

--- src/main/java/algorithm/seek_prime_12/SeekPrime.java
+++ src/main/java/algorithm/seek_prime_12/SeekPrime.java
@@ -4,6 +4,31 @@ public class SeekPrime {
 
     public static int solution(int n) {
 
-        return 0;
+        int answer = 0;
+        int[] numbers = new int[n + 1];
+
+        for (int i = 2; i <= n; i++) {
+            numbers[i] = i;
+        }
+
+        for (int i = 2; i < n; i++) {
+
+            if (numbers[i] == 0) {
+                continue;
+            }
+
+            for (int j = 2 * i; j <= n; j += i) {
+                numbers[j] = 0;
+            }
+        }
+
+        for (int number : numbers) {
+
+            if (number != 0) {
+                answer++;
+            }
+        }
+
+        return answer;
     }
 }
