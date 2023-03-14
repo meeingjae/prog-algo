ca0ec1f4d52a5790901771b1868fd0b5d51502d6
Minjae Kim <minjae.kim@mytrum.com>
2023-03-14 11:10:39 +0900

    feat: 22. remove min num

--- src/main/java/algorithm/remove_min_num_22/RemoveMinNum.java
+++ src/main/java/algorithm/remove_min_num_22/RemoveMinNum.java
@@ -1,9 +1,33 @@
 package algorithm.remove_min_num_22;
 
+import java.util.Arrays;
+
 public class RemoveMinNum {
 
     public static int[] solution(int[] arr) {
 
-        return new int[]{};
+        if (arr.length == 1) {
+            arr[0] = -1;
+            return arr;
+        }
+        int min = Integer.MAX_VALUE;
+        int cnt = 0;
+        for (int a : arr) {
+            if (min == a) {
+                cnt++;
+            }
+            if (min > a) {
+                cnt = 1;
+                min = a;
+            }
+        }
+        int index = 0;
+        for (int a : arr) {
+            if (a != min) {
+                arr[index++] = a;
+            }
+        }
+
+        return Arrays.copyOf(arr, arr.length - cnt);
     }
 }
