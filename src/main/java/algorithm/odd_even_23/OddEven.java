c85e126ea8583188918dcaad4225917972624c27
Minjae Kim <minjae.kim@mytrum.com>
2023-04-06 23:20:42 +0900

    docs: 23 - 짝수와 홀수 생성

---
+++ src/main/java/algorithm/odd_even_23/OddEven.java
@@ -0,0 +1,9 @@
+package algorithm.odd_even_23;
+
+public class OddEven {
+
+    public static String solution(int num) {
+
+        return null;
+    }
+}
---
+++ src/test/java/algorithm/odd_even_23/OddEvenTest.java
@@ -0,0 +1,30 @@
+package algorithm.odd_even_23;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class OddEvenTest {
+
+    @Test
+    void test1() {
+        //given:
+        final int num = 3;
+        //when:
+        final String expected = "Odd";
+        final String result = OddEven.solution(num);
+        //then:
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void test2() {
+        //given:
+        final int num = 20000;
+        //when:
+        final String expected = "Even";
+        final String result = OddEven.solution(num);
+        //then:
+        assertThat(result).isEqualTo(expected);
+    }
+}
